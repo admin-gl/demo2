c378daf92a3442ce05eb0e9215fed721c649d64b
admin-gl <gbolou@enssat.fr>
2020-11-03 12:58:12 +0100

    major changes to Money
    plus test implementation with junit dependency and continuous integration started pth github workflows

--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -17,4 +17,22 @@ public class Money {
                 ", currency='" + currency + '\'' +
                 '}';
     }
+
+    public Money add(Money that){
+        return new Money(this.amount + that.amount, this.currency);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount &&
+                Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
+    }
 }
