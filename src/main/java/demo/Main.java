43393a57a83ac4ebea2a4b8b16b9cc0906827af5
admin-gl <gbolou@enssat.fr>
2020-11-03 12:38:36 +0100

    added main
    modified pom to enable exec:java with mvn

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class Main {
+    public static void main(String[] args) {
+        Money amount = new Money(42, "EUR");
+        System.out.println(amount.toString());
+    }
+}
--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,5 +1,20 @@
 package demo;
 
-public class Money {
+import java.util.Objects;
 
+public class Money {
+    public int amount;
+    public String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
